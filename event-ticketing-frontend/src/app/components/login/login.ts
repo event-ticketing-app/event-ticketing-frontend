04f5827b4e67e7e23d84f86f403b6c56611d0e87
joeldc-dev <joeldc.dev@gmail.com>
2026-06-12 12:29:34 +0200

    Add Angular Material to login component

--- event-ticketing-frontend/src/app/components/login/login.ts
+++ event-ticketing-frontend/src/app/components/login/login.ts
@@ -2,12 +2,17 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Auth } from '../../services/auth';
 import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule,MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
+
+
 })
 export class Login 
 {
