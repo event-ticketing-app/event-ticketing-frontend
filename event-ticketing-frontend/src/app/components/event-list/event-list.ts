d977ed15c4b938ed00efde6fad305af51b557c93
joeldc-dev <joeldc.dev@gmail.com>
2026-07-30 12:45:58 +0200

    fix: resolve SSR hydration issue in EventList component

--- event-ticketing-frontend/src/app/components/event-list/event-list.ts
+++ event-ticketing-frontend/src/app/components/event-list/event-list.ts
@@ -1,13 +1,14 @@
-import { Component,OnInit } from '@angular/core';
+import { Component,OnInit, Inject, PLATFORM_ID, ChangeDetectorRef } from '@angular/core';
 import { EventService } from '../../services/event';
 import {EventResponse} from '../../services/event';
 import { MatCardModule } from '@angular/material/card';
-import { DatePipe } from '@angular/common';
-
+import { DatePipe, isPlatformBrowser } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-event-list',
-  imports: [MatCardModule,DatePipe],
+  imports: [MatCardModule,MatButtonModule,DatePipe,RouterLink],
   templateUrl: './event-list.html',
   styleUrl: './event-list.css',
 })
@@ -17,9 +18,13 @@ import { DatePipe } from '@angular/common';
 export class EventList  implements OnInit
 {
   events: EventResponse[] = [];
-  constructor(private eventService: EventService){}
+  constructor(private eventService: EventService,@Inject(PLATFORM_ID) private platformId: Object, private cdr: ChangeDetectorRef){}
   ngOnInit(){
-     this.eventService.getEvents().subscribe(response => { this.events = response})
+    if (!isPlatformBrowser(this.platformId)) return;
+     this.eventService.getEvents().subscribe(response => { 
+      this.events = response;
+      this.cdr.detectChanges();
+    })
   }
 
 
