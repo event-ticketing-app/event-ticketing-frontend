6e21d6689a60a9612dbe990ad9cad8096f91616c
joeldc-dev <joeldc.dev@gmail.com>
2026-07-30 12:46:15 +0200

    fix: resolve SSR hydration issue in EventDetail component

--- event-ticketing-frontend/src/app/components/event-detail/event-detail.ts
+++ event-ticketing-frontend/src/app/components/event-detail/event-detail.ts
@@ -1,28 +1,32 @@
-import { Component,OnInit } from '@angular/core';
+import { Component,OnInit, Inject, PLATFORM_ID, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EventResponse, EventService } from '../../services/event';
-import { DatePipe } from '@angular/common';
+import { DatePipe, isPlatformBrowser } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
 
 @Component({
   selector: 'app-event-detail',
-  imports: [DatePipe],
+  imports: [DatePipe,MatButtonModule],
   templateUrl: './event-detail.html',
   styleUrl: './event-detail.css',
 })
 
 
 export class EventDetail implements OnInit
-{constructor(private activatedRoute: ActivatedRoute, private eventService: EventService){}
+{constructor(private activatedRoute: ActivatedRoute, private eventService: EventService,@Inject(PLATFORM_ID) private platformId: Object, private cdr: ChangeDetectorRef){}
 
   event: EventResponse | null=null;
   
   ngOnInit(){
+    if (!isPlatformBrowser(this.platformId)) return;
+
     const id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
     this.eventService.getEventById(id).subscribe(response => {
       this.event = response;
+      this.cdr.detectChanges();
     })
   }
 }
